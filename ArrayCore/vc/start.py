82b337e4acdf88262767fa6ba612b473970ddc8b
Oplphgcsf <100408974+Oplphgcsf@users.noreply.github.com>
2022-03-16 10:19:17 +0530

    Update start.py

--- ArrayCore/vc/start.py
+++ ArrayCore/vc/start.py
@@ -3,14 +3,14 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
-from .. import vcbot, SUDO_USERS, HNDLR, hl, START_VID
+from .. import vcbot, SUDO_USERS, HNDLR, hl, START_VID, ALIVE_PIC
 
 # @vcbot.on_message(filters.user(SUDO_USERS) & filters.private & filters.command(["start"], prefixes=HNDLR))
 # async def start(_, e: Message):
    # video=START_VID,
   # await vcbot.send_video(e.chat.id, video, f"Vc Raid Bot Is Working Fine. \nSend `{hl}help` To Know Your Commands. \n\n< Powered By @ArrayCore >")
 
-Array = "https://te.legra.ph/file/045d419efd37fd56e8562.jpg"
+Array = ALIVE_PIC or "https://te.legra.ph/file/045d419efd37fd56e8562.jpg"
 Hn = "/"
 @vcbot.on_message(filters.private & filters.incoming & filters.command(['start'], prefixes=Hn))
 async def _start(_, ok: Message):
